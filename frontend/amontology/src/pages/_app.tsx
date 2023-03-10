19b33b09f33cb456f57846881709a7525e783889
samsmusa <samsmusa@outlook.com>
2023-03-10 10:27:13 +0600

    all are ready

--- frontend/amontology/src/pages/_app.tsx
+++ frontend/amontology/src/pages/_app.tsx
@@ -1,6 +1,67 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+import React, { FC } from "react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
+import { EmotionCache } from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { createEmotionCache } from "@/utils";
+import { MUIProvider } from "@/providers";
+import "slick-carousel/slick/slick.css";
+import "@/styles/globals.css";
+import "@/styles/react-slick.css";
+import { NextPageWithLayout } from "@/interfaces/layout";
+import { useRouter } from "next/router";
+// import 'slick-carousel/slick/slick-theme.css'
+const clientSideEmotionCache = createEmotionCache();
+
+type AppPropsWithLayout = AppProps & {
+  emotionCache: EmotionCache;
+  Component: NextPageWithLayout;
+};
+
+function App(props: AppPropsWithLayout) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    if (router.isReady) {
+      setLoading(false);
+    }
+  }, [router.isReady]);
+
+  return (
+    // <CacheProvider value={emotionCache}>
+    <React.Fragment>
+      {" "}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+            <title>React Coursespace</title>
+          </Head>
+          <MUIProvider>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} />)}
+          </MUIProvider>
+        </>
+      )}
+    </React.Fragment>
+    // </CacheProvider>
+  );
 }
+
+export default App;
